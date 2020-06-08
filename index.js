20afb25c47ea34169f51d9af526a8904fa177be4
Marvin Thai <marvinthai@yahoo.com>
2020-06-07 20:38:27 -0500

    Fixed Headers
    Changing headers for stats endpoints to work from https://github.com/swar/nba_api/issues/124

--- index.js
+++ index.js
@@ -53,14 +53,15 @@ formatData = (json, options) => {
 
 getDataFromNBA = (params, endpoint, options) => {
 	var headers = {
-		"Accept-Encoding": "Accepflate, sdch",
-		"Accept-Language": "he-IL,he;q=0.8,en-US;q=0.6,en;q=0.4",
-		"Cache-Control": "max-age=0",
-		Connection: "keep-alive",
-		Host: "stats.nba.com",
-		Referer: "http://stats.nba.com/",
-		"User-Agent":
-		"PostmanRuntime/7.4.0" 
+		'Host': 'stats.nba.com',
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
+		'Accept': 'application/json, text/plain, */*',
+		'Accept-Language': 'en-US,en;q=0.5',
+		'Referer': 'https://stats.nba.com/',
+		'Accept-Encoding': 'gzip, deflate, br',
+		'Connection': 'keep-alive',
+		'x-nba-stats-origin': 'stats',
+		'x-nba-stats-token': 'true'
 	};
 
 	var url = generateURL(params, endpoint);
